7da676f6f17d2a8a71163ec913c6fb70328ad6c7
Maria Ruiz de Molina <mariaruizdemolina@iMac-de-Maria.local>
2020-12-05 21:35:26 +0100

    add cors, (pruebas haciendo push desde otro ordenador)

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
-const mongoose =require('mongoose')
+const mongoose =require('mongoose');
+const cors = require('cors')
 
 const MongoURI = process.env.MongoURI || 'mongodb://localhost:27017/backmovie'
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 
 
 const PORT = process.env.PORT || 3001;
 
+app.get('/', (req, res) => {
+    res.send('proyecto backend movies react');
+});
 
 mongoose.connect(MongoURI,{
 useNewUrlParser: true,
