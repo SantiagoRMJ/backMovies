69385cbbe38efb4c5bc935446386e91f543058a5
Maria Ruiz de Molina <mariaruizdemolina@iMac-de-Maria.local>
2020-12-06 11:43:46 +0100

    arreglado mejor

--- components/usuario/router.js
+++ components/usuario/router.js
@@ -3,6 +3,6 @@ const router = require('express').Router();
 const userController = require('./controller');
 
 router.post('/', userController.registro);
-router.get('/user/login', userController.login);
+router.get('/login', userController.login);
 
 module.exports = router;
